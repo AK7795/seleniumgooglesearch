cbfa827ffe8ed7ef01d31d19ed6fd2d118078f90
AK7795 <akshaysingodi@gmail.com>
2022-03-30 22:27:12 +0530

    done5

--- test_ass.py
+++ test_ass.py
@@ -1,7 +1,10 @@
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+
 driver = webdriver.Chrome(ChromeDriverManager().install())
+i1 = "7795910241"
+i2 = "AKsh@7795"
 print("test case started")
 driver.maximize_window()
 driver.get("https://www.google.com/")
@@ -9,7 +12,15 @@ time.sleep(1)
 driver.find_element_by_name("q").send_keys("linkedin")
 time.sleep(1)
 driver.find_element_by_name("btnK").click()
-
-time.sleep(6)
+#driver.get("https://www.linkedin.com/login")
+time.sleep(1)
+driver.find_element_by_xpath("//h3").click()
+driver.find_element_by_id("email-or-phone").send_keys(i1)
+time.sleep(1)
+driver.find_element_by_id("password").send_keys(i2)
+time.sleep(1)
+#driver.find_element_by_xpath("//button[@type='submit']").click()
+driver.find_element_by_id("join-form-submit").click()
+time.sleep(10)
 driver.close()
 print("test case completed")
